e403e69c17fe103c544c299eedfb17eac3447b60
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-06-30 20:33:30 -0300

    Create ambiente.js

---
+++ aula 14/ambiente.js
@@ -0,0 +1,19 @@
+/*
+var c = 1 
+while(c <= 5) {
+    console.log
+    c++
+}
+*/
+/*
+for(c=1; c<=10;c++){
+    console.log(c)
+}
+*/
+
+console.log("Vai começar...")
+for(var c=1;c<=4;c++){
+    console.log(c)
+}
+
+console.log("Fim!"  )
