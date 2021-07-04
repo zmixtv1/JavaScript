1c00eae22c24813ea409e331040589562190efc3
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-07-04 14:32:41 -0300

    a

--- aula 16ex/ex001/script.js
+++ aula 16ex/ex001/script.js
@@ -1,1 +1,57 @@
-alert(`Olá`)
+let num = document.getElementById("num")
+let Lista = document.getElementById("valores")
+let res = document.getElementById("res")
+let valores = []
+
+function isNumber(n){
+    if (Number(n) >=1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+
+function inLista(n,l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    }else {
+        return false
+    }
+}
+
+
+function adicionar(){
+    if(isNumber(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement(`option`)
+        item.text = `valor ${num.value} adicionado`
+        Lista.appendChild(item)
+        res.innerHTML = ""
+    }else{
+        alert("Valor invalido! ou valor já encontrado na lista.")
+    }
+    num.value = ""
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        alert("Adicione valores antes de finalizar!")
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for(let pos in valores){
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+
+        res.innerHTML = ""
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+    }
+}
