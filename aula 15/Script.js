63e27502ed0ff9d7f6e33ca34b3b38cbfefa4a3f
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-07-04 12:19:34 -0300

    15

--- aula 15/Script.js
+++ aula 15/Script.js
@@ -1,3 +1,16 @@
-let num = [5,8,2,9,3]
+let num = [6,8,5,7,4,8]
+num.sort()
+console.log(num)
+/*
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`O Primeiro valoer é ${num[0]}`)
+/*
+/*for(let pos = 0; pos<num.length;pos++){
+    console.log(`${pos+1} Posição tem o valor ${num[pos]}`)
+}
+*/
+for(let pos in num){
+    console.log(`${pos} Posição tem o valor ${num[pos]}`)
+}
 
-console.log(`Nosso vetor é o ${num}`)
+console.log(num.indexOf(8))
---
+++ aula 15/vetor na tela.js
@@ -0,0 +1,5 @@
+let vetores = [8,1,7,4,2,9]
+vetores.sort()
+for(var cont= 0;cont<vetores.length;cont++){
+    console.log(`A posição ${cont+1} tem o valor ${vetores[cont]}`)
+}
