3b60a1078bf97c11642a6f30d3f129d9942d3bf0
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-07-04 14:43:54 -0300

    Update script.js

--- aula 16ex/ex001/script.js
+++ aula 16ex/ex001/script.js
@@ -42,16 +42,21 @@ function finalizar(){
         let total = valores.length
         let maior = valores[0]
         let menor = valores[0]
+        let soma = 0
+        let media = 0
         for(let pos in valores){
+            soma += valores[pos]
             if (valores[pos] > maior)
                 maior = valores[pos]
             if (valores[pos] < menor)
                 menor = valores[pos]
         }
-
+        media = soma/total
         res.innerHTML = ""
         res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     }
 }
