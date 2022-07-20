1ee122403fa586ee5639a8579c5ae4cb391b5e1b
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2022-07-19 23:55:08 -0300

    Estudando Rotas
    Estudando Rotas

--- CursoDeNodejs/aula4/app.js
+++ CursoDeNodejs/aula4/app.js
@@ -1,4 +1,22 @@
-const express = require("EXPRES")
+const express = require("express");
+const { Http2ServerRequest } = require("http2");
 const app = express();
 
-app.listen(8081)
+
+
+app.get("/", function(req,res){
+    res.send("Seja Bem Vindo ao meu app!!")
+})
+
+app.get("/sobre", function(req,res){
+    res.send("Minha pagina Sobre")
+})
+
+app.get("/Blog", function(req,res){
+    res.send("Bem Vindo ao meu Blog")
+})
+
+app.listen(8081, function(){
+    console.log("Servidor Rodando na rede 8081")
+});
+
